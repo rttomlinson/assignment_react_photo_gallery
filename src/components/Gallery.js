f198cd1ba971449e4a8b7778a5a087eb1993d52c
Renzo Tomlinson <rttomlinson@gmail.com>
2017-05-02 17:38:05 +0000

    Split gallery into component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,14 +10,33 @@ let photos = photoData.data;
 class Gallery extends Component {
   constructor() {
     super()
-
+    
+    
+    
+    this.filters = [];
+    photos.forEach((photo) => {
+        if (!this.filters.includes(photo.filter)) {
+            this.filters.push(photo.filter);
+        }
+    });
     this.state = { photos }
+    this.filterPhotos = this.filterPhotos.bind(this);
+  }
+  
+  filterPhotos(event) {
+      
+      //filter photos
+      let filter = event.target.value;
+      
+      this.state.photos = this.state.photos.filter((photo) => {
+          return photo.tags.includes(filter);
+      });
   }
 
   render() {
     return (
       <div className="Gallery">
-        <GalleryControls />
+        <GalleryControls onChange={this.filterPhotos} filters={this.filters}/>
         <GalleryDisplay photos={this.state.photos}/>
       </div>
     );
--- src/components/GalleryControls.js
+++ src/components/GalleryControls.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 
 class GalleryControls extends Component {
+    
+    render() {
+        
+        let options = this.props.tags.map(tag => (
+            <option value={tag.name}>{tag.name}</option>
+            ));
+        
+        
+        return (
+            <form>
+                <select onChange={this.props.filterPhotos} name="tag">
+                    {options}
+                </select>
+            </form>
+        );
+    }
+    
+    
 
 }
 
